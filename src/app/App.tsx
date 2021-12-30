7c55ea69ff3a3fca007c693b37064629ad90c582
sspr <marcinszuscik11@gmail.com>
2021-12-30 13:37:34 +0100

    Fix logic and syntax mistakes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,13 @@
 import { useIntl } from 'react-intl';
 
 export const App = () => {
-  const intl = useIntl();
+  let { formatMessage } = useIntl();
 
   return (
     <>
       <h2>This is a header 2</h2>
       <h3>This is a header 3</h3>
-      <p>This is translated statement: {intl.formatMessage({ id: 'test.helloWorld' })}</p>
+      <p>This is translated statement: {formatMessage({ id: 'test.helloWorld' })}</p>
     </>
   );
 };
