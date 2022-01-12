ef71cbb9c1ab8a3200b0acb8953f17a2f6d4c699
sspr <marcinszuscik11@gmail.com>
2022-01-12 19:35:30 +0100

    Add tests to header

---
+++ src/ui/header/Header.test.tsx
@@ -0,0 +1,70 @@
+import { render, screen } from '@testing-library/react';
+import { Header } from './Header';
+import { AppProviders } from 'providers/AppProviders';
+import { defaultLocale } from 'context/locale/defaultLocale';
+import { AppLocale } from 'context/locale/appLocale.enum';
+import { translations } from 'i18n/messages';
+
+describe('Header component', () => {
+  it('renders language switch', () => {
+    render(
+      <AppProviders>
+        <Header />
+      </AppProviders>,
+    );
+
+    const localeButtonEn = screen.getByText(AppLocale.en);
+    const localeButtonPl = screen.getByText(AppLocale.pl);
+
+    expect(localeButtonEn).toBeInTheDocument();
+    expect(localeButtonPl).toBeInTheDocument();
+  });
+
+  it('has default language button that is bold', () => {
+    render(
+      <AppProviders>
+        <Header />
+      </AppProviders>,
+    );
+
+    const localeButtonActive = screen.queryByText(defaultLocale);
+
+    expect(localeButtonActive).toHaveClass('active');
+    expect(localeButtonActive).toHaveStyle('font-weight: 600');
+  });
+
+  it('has language bautton that is not bold', () => {
+    render(
+      <AppProviders>
+        <Header />
+      </AppProviders>,
+    );
+
+    const localeButtonNotActive = screen.getByText(defaultLocale === AppLocale.en ? AppLocale.pl : AppLocale.en);
+
+    expect(localeButtonNotActive).toHaveClass('not-active');
+    expect(localeButtonNotActive).not.toHaveStyle('font-weight: 600');
+  });
+
+  it('changes locale when locale button that is not actvie is clicked', () => {
+    render(
+      <AppProviders>
+        <Header />
+      </AppProviders>,
+    );
+
+    const localeButtonNotActive = screen.getByText(defaultLocale === AppLocale.en ? AppLocale.pl : AppLocale.en);
+
+    localeButtonNotActive.click();
+
+    const clickedLocaleButton = screen.getByText(defaultLocale === AppLocale.en ? AppLocale.pl : AppLocale.en);
+    const searchButton = screen.getAllByRole('button')[0];
+
+    expect(clickedLocaleButton).toHaveClass('active');
+    expect(searchButton).toHaveTextContent(
+      defaultLocale === AppLocale.en
+        ? translations[AppLocale.pl]['header.searchButtonText']
+        : translations[AppLocale.en]['header.searchButtonText'],
+    );
+  });
+});
